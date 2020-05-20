a014a11768d1381a68f5eb770244332f7aadf2ff
AndreiOiegar <andrei.oiegar@gmail.com>
2020-05-20 23:37:48 +0300

    Few adjustments

--- Classes/Game.js
+++ Classes/Game.js
@@ -1,19 +1,21 @@
-class Game{
-    constructor(){
+class Game {
+    constructor() {
         this.gameContainer = this.showMap();
         this.player = new Player;
     }
 
-    showMap(){
-    const gameContainer = document.querySelector(".map");
-    const {style} = gameContainer;
-    style.width = "600px";
-    style.height = "600px";
-    style.backgroundColor = "#367536";
-    style.border = "5px solid black";
-    document.body.appendChild(gameContainer);
-    return gameContainer;
+    showMap() {
+        const gameContainer = document.querySelector(".map");
+        const {
+            style
+        } = gameContainer;
+        style.width = "600px";
+        style.height = "600px";
+        style.backgroundColor = "#367536";
+        style.border = "5px solid black";
+        document.body.appendChild(gameContainer);
+        return gameContainer;
     }
 }
 
-const game = new Game;
+const game = new Game();
--- Classes/Player.js
+++ Classes/Player.js
@@ -1,70 +1,79 @@
 class Player {
-    constructor() {
-    this.gameContainer;
+  constructor() {
     this.div = Player.display();
     this.move();
-    }
-  
-    // logica de miscare
-    move() {
-      console.log('miscare', this)
-      document.addEventListener('keyup', (event) => {
-        console.log(this)
-        switch(event.keyCode){
-            case 40 : //arrow down
-            const oldTop = parseInt(this.div.style.top);
+  }
+
+  // logica de miscare
+  move() {
+    document.addEventListener('keyup', (event) => {
+      const oldTop = parseInt(this.div.style.top);
+      const oldLeft = parseInt(this.div.style.left);
+      const stop = this.playerIsOutsideOfContainer();
+
+      switch (event.keyCode) {
+
+        case 40: //arrow down
+          if (stop === false) {
             this.div.style.top = `${oldTop + 20}px`;
-            console.log('arrow down', this.div.style.top);
-            break
+            // console.log('arrow down', this.div.style.top);
+          }
+          break;
 
-            case 39 : //arrow right
-            const oldLeft = parseInt(this.div.style.left);
+        case 39: //arrow right
+
+          if (stop === false) {
             this.div.style.left = `${oldLeft + 20}px`;
-            console.log("arrow right ", this.div.style.left) 
-            break
+            // console.log("arrow right ", this.div.style.left)
+          }
+          break;
 
-            case 38 : //arrow up
-            const oldBottom = parseInt(this.div.style.top);
-            this.div.style.top = `${oldBottom - 20}px`;
-            console.log("arrow up ", this.div.style.top) 
-            break
+        case 38: //arrow up
+          if (stop === false) {
+            this.div.style.top = `${oldTop - 20}px`;
+            // console.log("arrow up ", this.div.style.top) 
+          }
+          break;
 
-            case 37 : //arrow left
-            const oldRight = parseInt(this.div.style.left);
-            this.div.style.left = `${oldRight - 20}px`;
-            console.log("arrow right ", this.div.style.left) 
-            break
+        case 37: //arrow left
+          if (stop === false) {
+            this.div.style.left = `${oldLeft - 20}px`;
+            // console.log("arrow right ", this.div.style.left)
+          }
+          break;
 
-        }
-        this.playerIsOutsideOfContainer();
-      })
-    }
-  
-    // afisarea div-ului
-    static display() {
-        const gameContainer = document.querySelector(".map");
-        const div = document.createElement('div');
-        const { style } = div;
-        style.width = "20px";
-        style.height = "20px";
-        style.backgroundColor = "yellow";
-        style.position = "absolute";
-        style.top = "20px"
-        style.left = "20px"
-        style.borderRadius = "50%";
-    
-        gameContainer.appendChild(div);
-    
-        return div;
       }
 
-    playerIsOutsideOfContainer(){
-        const outsideHeight = parseInt(this.div.style.top);
-        const outsideWidth = parseInt(this.div.style.left);
+    })
+  }
 
-        if(outsideHeight === 0 || outsideHeight === 600 || outsideWidth === 0 || outsideWidth === 600){
-            alert("Game Over! Play inside the Map")
-            location.reload();
-        }
+  // afisarea div-ului
+  static display() {
+    const div = document.createElement('div');
+    const {
+      style
+    } = div;
+    style.width = "20px";
+    style.height = "20px";
+    style.backgroundColor = "yellow";
+    style.position = "absolute";
+    style.top = "30px"
+    style.left = "30px"
+    style.borderRadius = "50%";
+
+    document.body.appendChild(div);
+
+    return div;
+  }
+
+  playerIsOutsideOfContainer() {
+    const outsideHeight = parseInt(this.div.style.top);
+    const outsideWidth = parseInt(this.div.style.left);
+
+    if (outsideHeight === 10 || outsideHeight === 590 || outsideWidth === 10 || outsideWidth === 590) {
+      return true;
+    } else {
+      return false;
     }
   }
+}
